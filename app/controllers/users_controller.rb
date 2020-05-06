dcf0b240203fb3c64a89b0aa4f752d33d6390913
Naozumi Numata <naozumi225@gmail.com>
2020-05-06 14:47:13 +0900

    Add CRUD_app first times

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+    def index
+        @users = User.all
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        User.create(user_params)
+        redirect_to action: :index
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:name)
+    end
+
+end
